96b877db13e8078a747a029e550abb301804dd33
Sadhwika-123 <kondurisadhwika@gmail.com>
2025-01-08 07:00:18 +0000

    any name of the project

---
+++ day_2.py
@@ -0,0 +1,25 @@
+my_list = [10, 20, 30, 40, 50]
+print("List:", my_list)
+print("Element at index 2 in the List:", my_list[2])  # 30
+print("Element at index -1 in the List (last element):", my_list[-1])  # 50
+
+
+my_tuple = (5, 10, 15, 20, 25)
+print("\nTuple:", my_tuple)
+
+
+print("Element at index 1 in the Tuple:", my_tuple[1])  # 10
+print("Element at index -2 in the Tuple:", my_tuple[-2])  # 20
+
+my_dict = {
+    "name": "Alice",
+    "age": 25,
+    "city": "New York",
+    "job": "Engineer",
+    "country": "USA"
+}
+print("\nDictionary:", my_dict)
+
+
+print("Value for key 'age' in the Dictionary:", my_dict["age"])  # 25
+print("Value for key 'city' in the Dictionary:", my_dict["city"])  # New York

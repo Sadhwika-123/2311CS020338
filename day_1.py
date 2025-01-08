83f772e4a26ca94d625548b84cedb7f50ecde179
Sadhwika-123 <kondurisadhwika@gmail.com>
2025-01-08 06:53:57 +0000

    any name of the project

---
+++ day_1.py
@@ -0,0 +1,9 @@
+integer_var = 10
+float_var = 3.14
+string_var = "Hello, Jupyter!"
+boolean_var = True
+
+print("Integer Variable:", integer_var, "| Type:", type(integer_var))
+print("Float Variable:", float_var, "| Type:", type(float_var))
+print("String Variable:", string_var, "| Type:", type(string_var))
+print("Boolean Variable:", boolean_var, "| Type:", type(boolean_var))
